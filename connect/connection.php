39cfd76c2bc89e88a87f18b7a0336176f9524abf
Jash Bhalakia <jashbhalakia1@gmail.com>
2025-01-11 15:34:35 +0300

    Commit

--- connect/connection.php
+++ connect/connection.php
@@ -6,11 +6,4 @@ $dbPassword = "";
 $dbName = "loginsystem";
 
 $connect = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
-
-/*
-// Check connection
-if ($mysqli -> connect_error) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}*/
 ?>
