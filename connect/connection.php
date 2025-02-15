0bed0d2079de0e7b44d8e3a2bbba0667152a7184
Jash Bhalakia <jashbhalakia1@gmail.com>
2025-02-15 22:51:06 +0300

    New Connection

--- connect/connection.php
+++ connect/connection.php
@@ -1,9 +1,21 @@
-<?php 
+<?php
+class Connection {
+    private $host = "localhost"; // Change if necessary
+    private $dbname = "loginsystem"; // Change to your actual database name
+    private $username = "root"; // Change if using a different user
+    private $password = ""; // Change if your MySQL has a password
+    public $pdo;
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "loginsystem";
-
-$connect = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
+    public function __construct() {
+        try {
+            $this->pdo = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
 ?>
